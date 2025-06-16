876da75c700701cc27e9619a309cb312e24f8033
SecondNiceee <col1596321@gmail.com>
2025-06-16 17:28:33 +0300

    asd

--- src/views/Home/UI/sections/Generation.tsx
+++ src/views/Home/UI/sections/Generation.tsx
@@ -5,8 +5,8 @@ const Generation = () => {
         <section className='section relative'>
             <div className="absolute left-[0%] z-20 top-[30%] opacity-50 md:opacity-70 lg:opacity-100 bg-[#16cffd] w-[200px] h-[200px] blur-[200px]"></div>
             <div  className="container relative z-30 gap-[20px] p-container flex flex-col">
-                <h2 className='h2'>СмартКардио - забота о здоровье поколений!</h2>
-                <p className='mid-title text-black relative z-30'>стройство, которому доверяют врачи, пациенты и семьи. Сегодня — для себя. Завтра — для тех, кого мы любим.</p>
+                <h2 className='h2'>СмартКардио® - забота о здоровье поколений!</h2>
+                <p className='mid-title text-black relative z-30'>Устройство, которому доверяют врачи, пациенты и семьи. Сегодня — для себя. Завтра — для тех, кого мы любим.</p>
                 <Image width={1000} height={1000} alt="generation" className='max-w-[880px] w-full object-cover h-[300px] sm:h-[400px] md:h-[600px] mx-auto relative z-30'  src={"/images/generation.jpg"}  />
 
                 {/* <div className='relative'>
